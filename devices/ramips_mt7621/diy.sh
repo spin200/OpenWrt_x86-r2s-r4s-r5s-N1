03e9d2c4abe3c0ad89c4677b59d0257c49f247e2
spin200 <67430783+spin200@users.noreply.github.com>
2023-11-15 20:00:43 +0800

    Update diy.sh

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -3,7 +3,11 @@
 shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-
+# 修改系统欢迎词
+curl -fsSL https://raw.githubusercontent.com/spin200/HC5962-OpenWrt/master/server/etc/banner > package/base-files/files/etc/banner
+# 设置主机名称
+sed -i 's/OpenWrt/神奇的List/g' package/base-files/files/bin/config_generate
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='神奇的List'' package/lean/default-settings/files/zzz-default-settings
 
 sed -i "s/DEVICE_MODEL := HC5962$/DEVICE_MODEL := HC5962 \/ B70/" target/linux/ramips/image/mt7621.mk
 
